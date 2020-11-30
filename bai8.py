0cdd7af33ead7722e25ddf8c8821eaad0e074be8
khanhpt74 <72086102+khanhpt74@users.noreply.github.com>
2020-11-30 16:24:23 +0700

    Add files via upload

---
+++ bai8.py
@@ -0,0 +1,13 @@
+import getpass
+a = ('ngonhatkhanh@gmail.com')
+b = ("gui thay cuong")
+n = 5
+for a in range(5):
+    print("ngonhatkhanh1968@gmail.com")
+    for b in range(1):
+        print("gui thay cuong")
+
+
+
+
+
