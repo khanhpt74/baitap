31ecdf73a395128dc054f7781d5f0486a4070e92
khanhpt74 <72086102+khanhpt74@users.noreply.github.com>
2020-12-03 23:17:15 +0700

    Add files via upload

---
+++ baitap1.py
@@ -0,0 +1,13 @@
+list2 = [({'name': 'Peter', 'age':2}, {'name': 'John', 'age':21}), 
+         ({'name': 'Mary', 'age':2}, {'name': 'Trandanpro', 'age':21}), 
+         ({'name': 'Nam', 'age':2}, {'name': 'Hung', 'age':21}), 
+         ({'name': 'Mai', 'age':2}, {'name': 'Loan', 'age':21})]
+for list in list2:
+    for item in list:
+        print(item)
+        
+    
+        
+
+        
+           
