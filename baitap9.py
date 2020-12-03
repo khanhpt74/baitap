cf366f611fd59e4d372ac767780373832da402d3
khanhpt74 <72086102+khanhpt74@users.noreply.github.com>
2020-12-04 00:07:41 +0700

    Update baitap9.py

--- baitap9.py
+++ baitap9.py
@@ -1,3 +1,6 @@
+#- Xây dựng 1 vòng lặp không xác định thực hiện các việc sau:
+#+ Hỏi người dùng có muốn tắt máy không? nếu có thì thực hiện tắt máy
+# + Còn không thì đợi 30 giây sau mới hỏi lại câu hỏi ở trên. 
 import os 
 showdown = input("ban co muon tat may khong ? (co / khong): ")
 if showdown == "no":
